6accc672e0539d23bb825221ef3bab3782ba2d29
Aleksander Jaworski <aleck156@gmail.com>
2022-03-12 10:08:17 +0100

    alternative way of listening for escape key press

--- script.js
+++ script.js
@@ -19,8 +19,16 @@ overlay.addEventListener('click', toggleHidden);
 
 btnCloseModal.addEventListener('click', toggleHidden);
 
-document.onkeydown = function (evt) {
-  if (evt.key === 'Escape' && !overlay.classList.contains('hidden')) {
+// document.onkeydown = function (evt) {
+//   if (evt.key === 'Escape' && !overlay.classList.contains('hidden')) {
+//     toggleHidden();
+//   }
+// };
+
+// 'keydown' registers ESC key, whereas 'keypress' does not
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !overlay.classList.contains('hidden')) {
+    console.log(`${e.key} was pressed`);
     toggleHidden();
   }
-};
+});
