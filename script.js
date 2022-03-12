ac54023196bc90d67f2719f05a9bc9c3b9914e6d
Aleksander Jaworski <aleck156@gmail.com>
2022-03-12 07:35:15 +0100

    add ESCAPE key handling, refactor toggling hidden style class into a function

--- script.js
+++ script.js
@@ -2,12 +2,34 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
 
+const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const toggleHidden = function (objectName) {
+  objectName.classList.toggle('hidden');
+};
 
 btnOpenModal.forEach(element => {
   element.addEventListener('click', () => {
-    alert('wake up!');
+    toggleHidden(modal);
+    toggleHidden(overlay);
   });
 });
+
+overlay.addEventListener('click', () => {
+  toggleHidden(modal);
+  toggleHidden(overlay);
+});
+
+btnCloseModal.addEventListener('click', () => {
+  toggleHidden(modal);
+  toggleHidden(overlay);
+});
+
+document.onkeydown = function (evt) {
+  if (evt.key === 'Escape' && !overlay.classList.contains('hidden')) {
+    toggleHidden(modal);
+    toggleHidden(overlay);
+  }
+};
